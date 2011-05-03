537acb9739d83bd0ea76ad297abde283dfa6fd3c
Rupak Ganguly <rupakg@gmail.com>
2011-05-03 02:00:57 -0400

    Add another test for new project generation.

--- spec/cli/commands_spec.rb
+++ spec/cli/commands_spec.rb
@@ -57,9 +57,29 @@ describe "Ti Generator Commands" do
           remove_directories("HelloWorld")
         end
       end
-      context "with name Demo (ti new Demo)" do
+      context "with custom name (ti new Demo)" do
         it "should generate new project \"Demo\"" do
           response, status = capture_with_status(:stdout){ Ti::CLI.start(['new', 'Demo']) }
+          response.should eql("\e[1m\e[32mRemoving README file.\e[0m\e[0m\n\e[1m\e[32mRemoving Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the src directory.\e[0m\e[0m\n\e[1m\e[32mCreating the docs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the specs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/images directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/stylesheets directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/vendor directory.\e[0m\e[0m\n\e[1m\e[32mCreating Readme.mkd file.\e[0m\e[0m\n\e[1m\e[32mCreating src/app.coffee\e[0m\e[0m\n\e[1m\e[32mCreating .gitignore\e[0m\e[0m\n\e[1m\e[32mCreating config.rb\e[0m\e[0m\n\e[1m\e[32mCreating Rakefile\e[0m\e[0m\n\e[1m\e[32mCreating Guardfile\e[0m\e[0m\n\e[1m\e[32mYour Titanium project is ready for you to get rockin!\e[0m\e[0m\n")
+          status.should eql(Ti::CLI::STATUS_TYPES[:success])
+        end
+        after (:all) do
+          remove_directories("Demo")
+        end
+      end
+      context "with custom name and id (ti new Demo com.mycompany.demo)" do
+        it "should generate new project \"Demo\" with id \"com.mycompany.demo\"" do
+          response, status = capture_with_status(:stdout){ Ti::CLI.start(['new', 'Demo', 'com.mycompany.demo']) }
+          response.should eql("\e[1m\e[32mRemoving README file.\e[0m\e[0m\n\e[1m\e[32mRemoving Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the src directory.\e[0m\e[0m\n\e[1m\e[32mCreating the docs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the specs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/images directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/stylesheets directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/vendor directory.\e[0m\e[0m\n\e[1m\e[32mCreating Readme.mkd file.\e[0m\e[0m\n\e[1m\e[32mCreating src/app.coffee\e[0m\e[0m\n\e[1m\e[32mCreating .gitignore\e[0m\e[0m\n\e[1m\e[32mCreating config.rb\e[0m\e[0m\n\e[1m\e[32mCreating Rakefile\e[0m\e[0m\n\e[1m\e[32mCreating Guardfile\e[0m\e[0m\n\e[1m\e[32mYour Titanium project is ready for you to get rockin!\e[0m\e[0m\n")
+          status.should eql(Ti::CLI::STATUS_TYPES[:success])
+        end
+        after (:all) do
+          remove_directories("Demo")
+        end
+      end
+      context "with custom name, id and platform (ti new Demo com.mycompany.demo ipad)" do
+        it "should generate new project \"Demo\" with id \"com.mycompany.demo\" and for platform \"ipad\"" do
+          response, status = capture_with_status(:stdout){ Ti::CLI.start(['new', 'Demo', 'com.mycompany.demo']) }
           response.should eql("\e[1m\e[32mRemoving README file.\e[0m\e[0m\n\e[1m\e[32mRemoving Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the src directory.\e[0m\e[0m\n\e[1m\e[32mCreating the docs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the specs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/images directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/stylesheets directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/vendor directory.\e[0m\e[0m\n\e[1m\e[32mCreating Readme.mkd file.\e[0m\e[0m\n\e[1m\e[32mCreating src/app.coffee\e[0m\e[0m\n\e[1m\e[32mCreating .gitignore\e[0m\e[0m\n\e[1m\e[32mCreating config.rb\e[0m\e[0m\n\e[1m\e[32mCreating Rakefile\e[0m\e[0m\n\e[1m\e[32mCreating Guardfile\e[0m\e[0m\n\e[1m\e[32mYour Titanium project is ready for you to get rockin!\e[0m\e[0m\n")
           status.should eql(Ti::CLI::STATUS_TYPES[:success])
         end
