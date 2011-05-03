4432ca1508551f52a8a1f6d2174fddb76844751f
Rupak Ganguly <rupakg@gmail.com>
2011-05-03 00:45:43 -0400

    Add more tests for help command.

--- spec/cli/commands_spec.rb
+++ spec/cli/commands_spec.rb
@@ -4,7 +4,20 @@ describe "Ti Generator Commands" do
   context "Help command:" do
     context "ti help" do
       it "should display the basic help for the CLI" do
-        response, status = capture_with_status(:stdout){ Ti::CLI.start(['-help']) }
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['help']) }
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
+      end
+    end
+    context "ti help new" do
+      it "should display help for the task 'new'" do
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['help', 'new']) }
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
+      end
+    end
+    context "ti help invalid_task" do
+      it "should display appropriate error message" do
+        response, status = capture_with_status(:stderr){ Ti::CLI.start(['help', 'blah']) }
+        response.should eql("Could not find task \"blah\".\n")
         status.should eql(Ti::CLI::STATUS_TYPES[:success])
       end
     end
