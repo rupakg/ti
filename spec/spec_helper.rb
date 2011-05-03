c2f3c86437a64debf457819f07bfe6075cca336c
Rupak Ganguly <rupakg@gmail.com>
2011-05-02 23:52:58 -0400

    Add helper to capture response and status back from cli.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,22 @@ require 'cli'
 
 RSpec.configure do |config|
 
+  def capture_with_status(stream)
+    exit_status = 0
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      begin
+        yield
+      rescue SystemExit => system_exit # catch any exit calls
+        exit_status = system_exit.status
+      end
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+    return result, exit_status
+  end
+
 end
 
