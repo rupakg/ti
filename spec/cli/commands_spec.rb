3e4aa2a5735b799fe2dbe06911536e25844c7ffe
Rupak Ganguly <rupakg@gmail.com>
2011-05-03 01:46:11 -0400

    Add tests for new project creation.

--- spec/cli/commands_spec.rb
+++ spec/cli/commands_spec.rb
@@ -45,4 +45,28 @@ describe "Ti Generator Commands" do
       end
     end
   end
+  context "New command: " do
+    context "Generate a new project: " do
+      context "with defaults (ti new)" do
+        it "should generate new project \"HelloWorld\"" do
+          response, status = capture_with_status(:stdout){ Ti::CLI.start(['new']) }
+          response.should eql("\e[1m\e[32mRemoving README file.\e[0m\e[0m\n\e[1m\e[32mRemoving Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the src directory.\e[0m\e[0m\n\e[1m\e[32mCreating the docs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the specs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/images directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/stylesheets directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/vendor directory.\e[0m\e[0m\n\e[1m\e[32mCreating Readme.mkd file.\e[0m\e[0m\n\e[1m\e[32mCreating src/app.coffee\e[0m\e[0m\n\e[1m\e[32mCreating .gitignore\e[0m\e[0m\n\e[1m\e[32mCreating config.rb\e[0m\e[0m\n\e[1m\e[32mCreating Rakefile\e[0m\e[0m\n\e[1m\e[32mCreating Guardfile\e[0m\e[0m\n\e[1m\e[32mYour Titanium project is ready for you to get rockin!\e[0m\e[0m\n")
+          status.should eql(Ti::CLI::STATUS_TYPES[:success])
+        end
+        after (:all) do
+          remove_directories("HelloWorld")
+        end
+      end
+      context "with name Demo (ti new Demo)" do
+        it "should generate new project \"Demo\"" do
+          response, status = capture_with_status(:stdout){ Ti::CLI.start(['new', 'Demo']) }
+          response.should eql("\e[1m\e[32mRemoving README file.\e[0m\e[0m\n\e[1m\e[32mRemoving Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources directory.\e[0m\e[0m\n\e[1m\e[32mCreating the src directory.\e[0m\e[0m\n\e[1m\e[32mCreating the docs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the specs directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/images directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/stylesheets directory.\e[0m\e[0m\n\e[1m\e[32mCreating the Resources/vendor directory.\e[0m\e[0m\n\e[1m\e[32mCreating Readme.mkd file.\e[0m\e[0m\n\e[1m\e[32mCreating src/app.coffee\e[0m\e[0m\n\e[1m\e[32mCreating .gitignore\e[0m\e[0m\n\e[1m\e[32mCreating config.rb\e[0m\e[0m\n\e[1m\e[32mCreating Rakefile\e[0m\e[0m\n\e[1m\e[32mCreating Guardfile\e[0m\e[0m\n\e[1m\e[32mYour Titanium project is ready for you to get rockin!\e[0m\e[0m\n")
+          status.should eql(Ti::CLI::STATUS_TYPES[:success])
+        end
+        after (:all) do
+          remove_directories("Demo")
+        end
+      end
+    end
+  end
 end
