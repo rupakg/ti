d935ee2b2d986d135c7f3b96214dc6f7b48727c9
Rupak Ganguly <rupakg@gmail.com>
2011-05-02 23:51:58 -0400

    Refactor tests to include response and status checks. Add test for Help command.

--- spec/cli/commands_spec.rb
+++ spec/cli/commands_spec.rb
@@ -1,10 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Ti Generator Commands" do
+  context "Help command:" do
+    context "ti help" do
+      it "should display the basic help for the CLI" do
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['-help']) }
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
+      end
+    end
+  end
   context "Info command:" do
     context "ti info" do
       it "should display the version" do
-        Ti::CLI.start(['info'])
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['info']) }
+        response.should eql("version: 0.2.0\n")
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
+      end
+    end
+    context "ti -v (alias)" do
+      it "should display the version" do
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['-v']) }
+        response.should eql("version: 0.2.0\n")
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
+      end
+    end
+    context "ti --version (alias)" do
+      it "should display the version" do
+        response, status = capture_with_status(:stdout){ Ti::CLI.start(['--version']) }
+        response.should eql("version: 0.2.0\n")
+        status.should eql(Ti::CLI::STATUS_TYPES[:success])
       end
     end
   end
